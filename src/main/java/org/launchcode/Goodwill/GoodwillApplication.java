cbbeed7b559ba1783f89bcaf9a76f4cfc97a0ea1
vaishnavi <vaishu.srini15@gmail.com>
2021-11-19 05:21:59 -0600

    first commit

---
+++ src/main/java/org/launchcode/Goodwill/GoodwillApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Goodwill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodwillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodwillApplication.class, args);
+	}
+
+}
